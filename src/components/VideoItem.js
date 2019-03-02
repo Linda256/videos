734b12e972e5dcdd466084d59075e1d8efaaadbe
Linda Wang <lindawang256@gmail.com>
2019-03-02 10:52:21 -0800

    add semantic ui

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const VideoItem=(props)=>{
+    console.log("props in VideoItem:",props.video.snippet.title);
     return(
-        <div>
-            <p>Video</p>
-           
+        <div className="ui card">
+            <div className="image">
+                <img 
+                    src={props.video.snippet.thumbnails.medium.url}
+                />
+            </div>
+            <div className="content">
+                <a className="header">{props.video.snippet.title}</a> 
+                <div className="description">
+                {props.video.snippet.description}
+                </div>
+            </div>
         </div>
     )
     
