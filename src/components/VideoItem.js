eda7946d777ff6006edec9c48ab3e3155a76c5b8
Linda Wang <lindawang256@gmail.com>
2019-05-26 20:44:57 -0700

    Clean code

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,41 +1,27 @@
 import React from 'react';
 
 const VideoItem=(props)=>{
-    console.log("props in VideoItem:",props.video.snippet.title);
-    
+    //console.log("props in VideoItem:",props.video.snippet.title);
     const getCurrentVideo=()=>{
         const currVideo=props.video;
         props.handleSelect(currVideo);
     }
-    // return(
-    //     <div className="ui card" onClick={()=>getCurrentVideo()}>
-    //         <div className="image" >
-    //             <img 
-    //                 alt={props.video.snippet.description}
-    //                 src={props.video.snippet.thumbnails.high.url}
-    //             />
-    //         </div>
-    //         <div className="content">
-    //             <a className="header">{props.video.snippet.title}</a> 
-    //             <div className="description">
-    //             {props.video.snippet.description}
-    //             </div>
-    //         </div>
-    //     </div>
-    // )
     return(
-        <div className="item">
-            <img className="ui image" 
-                alt={props.video.snippet.description}
-                src={props.video.snippet.thumbnails.medium.url}
-            />
+        <div className="ui card" onClick={()=>getCurrentVideo()}>
+            <div className="image" >
+                <img 
+                    alt={props.video.snippet.description}
+                    src={props.video.snippet.thumbnails.high.url}
+                />
+            </div>
             <div className="content">
-                <a className="header">{props.video.snippet.title}</a>
-                <div className="description">{props.video.snippet.description}</div>
+                <a className="header">{props.video.snippet.title}</a> 
+                <div className="description">
+                {props.video.snippet.description}
+                </div>
             </div>
         </div>
-    )
-    
+    )  
 }
 
 export default VideoItem;
